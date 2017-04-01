0d836fc2bda829f012c8347ff7c2a1804aa5116d
Ethien Salinas <ethien.salinas@gmail.com>
2017-04-01 15:55:01 -0600

    ADD ajax call with only name

---
+++ js/freelancer.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+	
+	// read the text of an element (get)
+	console.log($('h1').text());
+
+	// modify text (set)
+	$('h1').text('Starting with jQuery');
+
+	// manipulate DOM adding a new element
+	$( "<p>New element with jQuery</p>" ).appendTo('h1');
+
+	// Activate ScrollSpy
+	$('body').scrollspy({
+		target: '.navbar-custom',
+		offset: 51
+	});
+
+
+	$('form#contactForm').on( "submit",function (event) {
+		event.preventDefault();
+		let name = $('input#name').val();
+		console.log(`##### ${name} #####`);
+
+		$.ajax({
+			url: 'http://localhost:4000/mail',
+			type: 'POST',
+			data:{
+				name: name
+			},
+			success: function(response){
+				console.log('EXITO!!!' + response);
+			},
+			error: function (error) {
+				console.log("FAIL!!! :'(" + error);
+			}
+		});
+	});
+});
