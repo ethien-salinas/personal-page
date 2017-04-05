a432954405b45f88c994fcc3a29536c055d19b84
Ethien Salinas <ethien.salinas@gmail.com>
2017-04-05 07:50:15 -0500

    ADD vue to render modals

--- js/controller.js
+++ js/controller.js
@@ -57,5 +57,71 @@ let portfolioCtrl = new Vue({
   }
 });
 
-
+let portfolioDetail = new Vue({
+  el: '#portfolioDetailSection',
+  data: {
+    portfolioDetails:[
+      {
+        "id": "portfolioModal1",
+        "title": "Data integration project",
+        "description": "Bacon ipsum dolor amet swine burgdoggen chicken, pork andouille ribeye meatball jowl landjaeger pork belly alcatra doner jerky. Kevin flank venison, shoulder pastrami cupim leberkas. Ground round jerky landjaeger andouille filet mignon beef. Cupim doner rump tri-tip drumstick ball tip picanha biltong, bacon leberkas alcatra chuck tail. Venison boudin prosciutto chicken ham hock, pork chop doner salami. Sirloin biltong swine turkey, leberkas chicken corned beef strip steak tenderloin hamburger jowl turducken t-bone spare ribs shank.",
+        "img": "cabin.png",
+        "client": "Atom",
+        "date": "2017-01-01",
+        "service": "Develop of the atom tool",
+        "project_link": "https://atom.io/"
+      },
+      {
+        "id": "portfolioModal2",
+        "title": "Data integration project",
+        "description": "Bacon ipsum dolor amet swine burgdoggen chicken, pork andouille ribeye meatball jowl landjaeger pork belly alcatra doner jerky. Kevin flank venison, shoulder pastrami cupim leberkas. Ground round jerky landjaeger andouille filet mignon beef. Cupim doner rump tri-tip drumstick ball tip picanha biltong, bacon leberkas alcatra chuck tail. Venison boudin prosciutto chicken ham hock, pork chop doner salami. Sirloin biltong swine turkey, leberkas chicken corned beef strip steak tenderloin hamburger jowl turducken t-bone spare ribs shank.",
+        "img": "cake.png",
+        "client": "Atom",
+        "date": "2017-01-01",
+        "service": "Develop of the atom tool",
+        "project_link": "https://atom.io/"
+      },
+      {
+        "id": "portfolioModal3",
+        "title": "Data integration project",
+        "description": "Bacon ipsum dolor amet swine burgdoggen chicken, pork andouille ribeye meatball jowl landjaeger pork belly alcatra doner jerky. Kevin flank venison, shoulder pastrami cupim leberkas. Ground round jerky landjaeger andouille filet mignon beef. Cupim doner rump tri-tip drumstick ball tip picanha biltong, bacon leberkas alcatra chuck tail. Venison boudin prosciutto chicken ham hock, pork chop doner salami. Sirloin biltong swine turkey, leberkas chicken corned beef strip steak tenderloin hamburger jowl turducken t-bone spare ribs shank.",
+        "img": "circus.png",
+        "client": "Atom",
+        "date": "2017-01-01",
+        "service": "Develop of the atom tool",
+        "project_link": "https://atom.io/"
+      },
+      {
+        "id": "portfolioModal4",
+        "title": "Data integration project",
+        "description": "Bacon ipsum dolor amet swine burgdoggen chicken, pork andouille ribeye meatball jowl landjaeger pork belly alcatra doner jerky. Kevin flank venison, shoulder pastrami cupim leberkas. Ground round jerky landjaeger andouille filet mignon beef. Cupim doner rump tri-tip drumstick ball tip picanha biltong, bacon leberkas alcatra chuck tail. Venison boudin prosciutto chicken ham hock, pork chop doner salami. Sirloin biltong swine turkey, leberkas chicken corned beef strip steak tenderloin hamburger jowl turducken t-bone spare ribs shank.",
+        "img": "game.png",
+        "client": "Atom",
+        "date": "2017-01-01",
+        "service": "Develop of the atom tool",
+        "project_link": "https://atom.io/"
+      },
+      {
+        "id": "portfolioModal5",
+        "title": "Data integration project",
+        "description": "Bacon ipsum dolor amet swine burgdoggen chicken, pork andouille ribeye meatball jowl landjaeger pork belly alcatra doner jerky. Kevin flank venison, shoulder pastrami cupim leberkas. Ground round jerky landjaeger andouille filet mignon beef. Cupim doner rump tri-tip drumstick ball tip picanha biltong, bacon leberkas alcatra chuck tail. Venison boudin prosciutto chicken ham hock, pork chop doner salami. Sirloin biltong swine turkey, leberkas chicken corned beef strip steak tenderloin hamburger jowl turducken t-bone spare ribs shank.",
+        "img": "safe.png",
+        "client": "Atom",
+        "date": "2017-01-01",
+        "service": "Develop of the atom tool",
+        "project_link": "https://atom.io/"
+      },
+      {
+        "id": "portfolioModal6",
+        "title": "Data integration project",
+        "description": "Bacon ipsum dolor amet swine burgdoggen chicken, pork andouille ribeye meatball jowl landjaeger pork belly alcatra doner jerky. Kevin flank venison, shoulder pastrami cupim leberkas. Ground round jerky landjaeger andouille filet mignon beef. Cupim doner rump tri-tip drumstick ball tip picanha biltong, bacon leberkas alcatra chuck tail. Venison boudin prosciutto chicken ham hock, pork chop doner salami. Sirloin biltong swine turkey, leberkas chicken corned beef strip steak tenderloin hamburger jowl turducken t-bone spare ribs shank.",
+        "img": "submarine.png",
+        "client": "Atom",
+        "date": "2017-01-01",
+        "service": "Develop of the atom tool",
+        "project_link": "https://atom.io/"
+      }
+    ]
+  }
+})
 
