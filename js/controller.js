134adcc9c00369e314cccbbbb36dcbce4b849b58
Ethien Salinas <ethien.salinas@gmail.com>
2017-04-07 07:42:06 -0500

    ADD remote call to get data

--- js/controller.js
+++ js/controller.js
@@ -1,21 +1,48 @@
-
+const BASE_URL = 'http://localhost:4000'
 let menuCtrl = new Vue({
   el: '#collapse-menu',
   data: {
-    menuItems: menuItemsArray
+    menuItems: []
+  },
+  beforeCreate() {
+      let xhr = new XMLHttpRequest()
+      let self = this
+      xhr.open('GET', `${BASE_URL}/portfolio`)
+      xhr.onload = function () {
+        self.menuItems = JSON.parse(xhr.responseText).menuItemsArray
+      }
+      xhr.send()
   }
 });
 
 let portfolioCtrl = new Vue({
   el: '#portfolio',
-  data: {
-    portfolioItems: portfolioArray
+  data:{
+    portfolioItems:[]
+  },
+  beforeCreate() {
+      let xhr = new XMLHttpRequest()
+      let self = this
+      xhr.open('GET', `${BASE_URL}/portfolio`)
+      xhr.onload = function () {
+        self.portfolioItems = JSON.parse(xhr.responseText).portfolioArray
+      }
+      xhr.send()
   }
 });
 
 let portfolioDetail = new Vue({
   el: '#portfolioDetailSection',
   data: {
-    portfolioDetails:portfolioDetailsArray
+    portfolioDetails:[]
+  },
+  beforeCreate() {
+      let xhr = new XMLHttpRequest()
+      let self = this
+      xhr.open('GET', `${BASE_URL}/portfolio`)
+      xhr.onload = function () {
+        self.portfolioDetails = JSON.parse(xhr.responseText).portfolioDetailsArray
+      }
+      xhr.send()
   }
 })
